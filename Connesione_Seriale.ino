503f9bab9185bd444ace7fc6384fe40cc4b486c1
MengaFrancesco <57190306+MengaFrancesco@users.noreply.github.com>
2020-01-11 12:40:48 +0100

    Add files via upload

---
+++ Connesione_Seriale.ino
@@ -0,0 +1,61 @@
+void setup() {
+
+  /* Inizializzazione seriale */
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  /* Aspetta carattere G e operazione */
+  while(Serial.available()<2);
+  char g = Serial.read();
+  char op = Serial.read();
+
+  /* DEBUG */
+  Serial.print(g);
+  Serial.print(op);
+
+  if(op=='0' || op=='1')
+  {
+    /* Aspetta 4 caratteri */
+    while(Serial.available()<4);
+    
+    byte value0 = Serial.read();
+    byte value1 = Serial.read();
+    int X = (int)value0+(int)value1;
+    
+    value0 = Serial.read();
+    value1 = Serial.read();
+    int Y = (int)value0+(int)value1;
+
+    /* DEBUG */
+    Serial.print(X);
+    Serial.print(Y);
+    
+    if(op=='0')
+    {} /* Spostamento... */
+    else 
+    {} /* Spostamento con penna abbassata.. */
+    
+  }
+  else if(op=='2')
+  {
+    int X=0;
+    int Y=0;
+
+    /* DEBUG */
+    Serial.print(X);
+    Serial.print(Y);
+    
+    /* Spostamento.... */
+  }
+
+  
+  /* Scrive OK sulla seriale */
+  if(op=='0' || op=='1' || op=='2')
+  {
+    Serial.print('O');
+    Serial.print('K');
+  }
+  
+}
