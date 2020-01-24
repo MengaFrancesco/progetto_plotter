e7bd473e95b4742531a266151d370ccc3a1b1224
Deloooo <57558338+Deloooo@users.noreply.github.com>
2020-01-24 19:26:59 +0100

    Versione 2 (Più corretta)

--- Step_motor.ino
+++ Step_motor.ino
@@ -39,26 +39,26 @@ void setup()
 void loop()
 {
 
-  / * Aspetta carattere G e operazione * /
+  // Aspetta carattere G e operazione 
   while (Serial. available () < 2 );
   char g = Serial. read ();
   char op = Serial. read ();
 
-  / * DEBUG * /
+  // DEBUG 
   Serial.println (op);
   Serial.println (g);
 
   if (op == ' 0 ' || op == ' 1 ' )
   {
-    / * Aspetta 4 caratteri * /
+    //Aspetta 4 caratteri 
     while (Serial. available () < 4 );
     
     byte value0 = Serial. read ();
     byte value1 = Serial. read ();
     int X = ( int ) value0 + ( int ) value1;
     
-    valore0 = Serial. read ();
-    value1 = Serial. read ();
+    byte valore2 = Serial.read();
+    byte value3 = Serial.read ();
     int Y = ( int ) value0 + ( int ) value1;
 
     
@@ -72,11 +72,11 @@ void loop()
   {
     int X = 0 ;
     int Y = 0 ;
-    spostati(X, Y);
+    spostati(X,Y);
   }
 
   
-  / * Scrive OK sulla seriale * /
+  // Scrive OK sulla seriale 
   if (op == ' 0 ' || op == ' 1 ' || op == ' 2 ' )
   {
     Serial. print ( ' O ' );
